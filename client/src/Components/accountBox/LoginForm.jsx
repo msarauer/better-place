93ea3e2a89eea65edd24c18720d46131fdde4573
Cecil <cvogrinetz@gmail.com>
2021-07-04 00:53:53 -0700

    Add tweaks to spacing for login form

--- client/src/Components/accountBox/LoginForm.jsx
+++ client/src/Components/accountBox/LoginForm.jsx
@@ -16,11 +16,14 @@ const LoginForm = (props) => {
     <BoxContainer>
       <FormContainer>
         <Input type="email" placeholder="Email"/>
+
+      <Marginer direction="vertical" margin={10} />
         <Input type="password" placeholder="Password" />
       </FormContainer>
-      <Marginer direction="vertical" margin={10} />
+      <Marginer direction="vertical" margin={15} />
       <MutedLink href="#">Forgot Your password?</MutedLink>
-      <Marginer direction="vertical" margin='1.6em' />
+      <Marginer direction="vertical" margin={60} />
+
       <SubmitButton type="submit">SignIn</SubmitButton>
       <Marginer direction="vertical" margin='1.6em' />
       <MutedLink href="#">Dont have an account? <BoldLink href='#' onClick={switchToSignup}>SignUp</BoldLink></MutedLink>
